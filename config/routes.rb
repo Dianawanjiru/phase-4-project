2d39877595afde33ebfdaaac319e856880bd13dc
Diana <diana.wanjiru@student.moringaschool.com>
2022-12-07 12:06:57 +0300

    new react setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get '/hello', to: 'application#hello_world'
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
