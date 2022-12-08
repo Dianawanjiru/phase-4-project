43e95ff207d8f6b1e8c5501c28cdd9906b5fa821
Diana <diana.wanjiru@student.moringaschool.com>
2022-12-08 12:01:39 +0300

    update post backend

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,3 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :"image-url", :description
+  attributes :id, :title, :image_url, :description, :user_id
 end
--- db/migrate/20221208090048_create_posts.rb
+++ db/migrate/20221208090048_create_posts.rb
@@ -2,8 +2,9 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :image-url
+      t.string :image_url
       t.text :description
+      t.integer :user_id
 
       t.timestamps
     end
